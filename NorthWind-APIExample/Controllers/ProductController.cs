e5f7031530134ed9a58925088f8ef1c2d0f91182
Sabrina Obed Gill <sabrina.yousaf31@gmail.com>
2025-04-08 14:30:29 +0200

    Created a Product controller class and used DI

---
+++ NorthWind-APIExample/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using AutoMapper;
+using NorthWind_APIExample.Models;
+using NorthWind_APIExample.Dto;
+
+
+namespace NorthWind_APIExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly NorthwindContext _context;
+        private readonly IMapper _mapper;
+
+        public ProductController(NorthwindContext context, IMapper mapper)
+        {
+             _context = context;
+            _mapper = mapper;
+
+        }
+    }
+}
